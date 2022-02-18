db8f2030677f0ea14e85588fcb141551aa447198
unknown <zakariae.dinar@gmail.com>
2022-02-18 18:13:09 +0100

    Update routing

--- routes/botman.php
+++ routes/botman.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
+// $botman->hears('Hi', function ($bot) {
+//     $bot->reply('Hello!');
+// });
 
-$botman->hears('Salam', BotManController::class . "@askForHelp");
+$botman->hears('Hi', BotManController::class . "@askForHelp");
+$botman->hears('Hello', BotManController::class . "@askForHelp");
 
 $botman->hears('Start conversation', BotManController::class . '@startConversation');
